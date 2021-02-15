b1e45783738bbbe372dfd74dac3dcdda20b80254
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2021-02-15 14:03:16 -0500

    Updating mobile card
    Due to long text lengths the mobile view is now using the default card from the Busrides home page which allows for more space

--- src/LearningPathCard.js
+++ src/LearningPathCard.js
@@ -1,22 +1,42 @@
+import React from 'react';
+import { useWindowWidth } from '@react-hook/window-size';
 import styles from './LearningPathCard.module.css';
 
 export default function LearningPathCard(props) {
 
+    const width = useWindowWidth();
+
     return (
-        <a className={styles.link} href={props.cardContent.link} target="_blank">
-            <div className={styles.card}>
-                <img className={styles.image} src={props.cardContent.imageLink} alt={props.cardContent.imageAltTxt}/>
-                <div className={styles.content}>
-                    <div className={styles.topContent}>
-                        <p className={styles.tag}>{props.cardContent.tagTitle}</p>
-                        <p className={styles.timeStamps}>{props.cardContent.platform}<span>•</span>{props.cardContent.readTime}</p>
+        <React.Fragment>
+            {width < 615 ?
+                <a className={styles.mobile_link} href={props.cardContent.link} target="_blank">
+                    <div className={styles.mobile_card}>
+                    <div className={styles.mobile_image} style={{backgroundImage: `url(${props.cardContent.imageLink})`}}/>
+                        <div className={styles.mobile_content}>
+                            <p className={styles.mobile_tag}>{props.cardContent.tagTitle}</p>
+                            <h2 className={styles.mobile_title}>{props.cardContent.title}</h2>
+                            <p className={styles.mobile_description}>{props.cardContent.description}</p>
+                            <p className={styles.mobile_timeStamps}>{props.cardContent.platform}<span>•</span>{props.cardContent.readTime}</p>
+                        </div>
                     </div>
-                    <div className={styles.bottomContent}>
-                        <h2 className={styles.title}>{props.cardContent.title}</h2>
-                        <p className={styles.description}>{props.cardContent.description}</p>
+                </a>  
+            :
+            <a className={styles.link} href={props.cardContent.link} target="_blank">
+                <div className={styles.card}>
+                    <img className={styles.image} src={props.cardContent.imageLink} alt={props.cardContent.imageAltTxt}/>
+                    <div className={styles.content}>
+                        <div className={styles.topContent}>
+                            <p className={styles.tag}>{props.cardContent.tagTitle}</p>
+                            <p className={styles.timeStamps}>{props.cardContent.platform}<span>•</span>{props.cardContent.readTime}</p>
+                        </div>
+                        <div className={styles.bottomContent}>
+                            <h2 className={styles.title}>{props.cardContent.title}</h2>
+                            <p className={styles.description}>{props.cardContent.description}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </a>
+            </a>
+            }
+        </React.Fragment>
     );
 }
